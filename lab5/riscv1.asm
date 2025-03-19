021db61a6ce79f3da700754ae6d9f0cd6f760f96
laurie-hxf <1847238294@qq.com>
2025-03-19 16:09:17 +0800

    lab5 partise 2-1 finished

---
+++ lab5/riscv1.asm
@@ -0,0 +1,26 @@
+.include "macro_print_str.asm"
+.data
+dvalue1: .word 0x03
+dvalue2: .word 0x80000000
+.text
+	lw t1, dvalue1
+	lw t2, dvalue2
+	sub t0, t1, t2 # add two values  t1-t2=t0      				t1-   t2-
+	slti t3, t2, 0 # t3 = (t2 < 0)						t1-   t2+
+	slt t4, t0, t1  # t4 = (t0 < t1), thst is, (t1 + t2 < t1)		t1+   t2-   
+									#	t1+   t2+
+			
+	mv a0, t0 
+	li a7, 1
+	ecall
+	beq  t3, t4, overflow    # print the sum
+				# overflow if (t2 < 0) && (t1 - t2 >= t1)
+				# or if (t2 >= 0) && (t1 + t2 < t1)
+	print_string("\nNo overflow occured.")
+	jal exit
+	
+overflow:
+	print_string("\nOne overflow occured.")
+	
+exit:
+	end
